820662bbf72f06b837840db63983273f4fe51bea
Michael J. Cox <mike.jon.cox@gmail.com>
2016-07-31 15:58:25 -0600

    removed wildcard route

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/images',express.static(__dirname + '/app/images'));
 app.use('/styles',express.static(__dirname + '/app/styles'));
 app.use('/views',express.static(__dirname + '/app/views'));
 
-app.use('/*', function(req, res) {
+app.use('/', function(req, res) {
 	console.log('Catch-all triggered');
 	console.log(req.baseUrl);
 	res.sendFile(__dirname + '/app/index.html');
